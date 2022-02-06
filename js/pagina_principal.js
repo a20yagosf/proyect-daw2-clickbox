191919d5b6208b3365c0d281c81c854489ff8f85
Irea Vila Ramilo <irea.vila.ramilo@gmail.com>
2022-02-06 20:30:30 +0100

    Cambios creación diseño js y pruebas de tipografia

--- js/pagina_principal.js
+++ js/pagina_principal.js
@@ -9,29 +9,17 @@ function crearDetallesSvg() {
     let svg = document.getElementById("designPantalla");
     //getBoundingClientRect me devuelve un objeto rect que es el rectángulo más pequeño que contenga tanto padding como bordes del div suscripciones, de ahí saco su heigh y posición (x,y)
     let recuadroSusc = document.getElementById("suscripciones").getBoundingClientRect();
-    //Creo cada uno de los elementos del svg
-    //Creo 3 circulos que se ponen encima del recuadro de suscripciones
-    let xSusc = 0;
-    //Calculo su Y mediante la altura del heigh del recuadro de suscr
     let ySusc = (recuadroSusc.y - (recuadroSusc.height / 2) + 1);
     let xFinSusc = (recuadroSusc.width / 0.8) / 3;
-    curvatura = xFinSusc;
-    let lineaPath = `M${xSusc}, ${ySusc} a${curvatura}, ${curvatura} 0 0, 1 ${xFinSusc}, 0 Z`;
-    svg.append(crearPath(lineaPath));
-    //Diferencia que le quitaremos para hacer el recuadro que restaremos más pequeño
-    //A la vez que creamos los rectámngulos creamos también los que los "restarán" con la clase restar
-    for(let i = 0; i < 3; i++){
-        let curvatura = i % 2 != 0 ? xFinSusc - 200 : xFinSusc;
-        let lineaPath = `M${xSusc}, ${ySusc} a${curvatura}, ${curvatura} 0 0, 1 ${xFinSusc}, 0 Z`;
-        svg.append(crearPath(lineaPath));
-        //Diferencia que le quitaremos para hacer el recuadro que restaremos más pequeño
-        let restaCurvatura = 80;
-        let desplazamiento = 80;
-        let curvaturaResta = `M${xSusc + restaCurvatura}, ${ySusc} a${curvatura}, ${curvatura} 0 0, 1 ${xFinSusc - restaCurvatura * 2}, 0 l-${desplazamiento}, 0 a${curvatura}, ${curvatura} 0 0, 0, -${(xFinSusc - desplazamiento)/2}, -0 z`;
-        svg.append(crearPath(curvaturaResta, "restar"));
-        xSusc += xFinSusc;
-    }
-    
+    //Creamos cada uno de los elementos del svg
+    //Creamos 3 circulos que se ponen encima del recuadro de suscripciones
+    crearDisenho(0, recuadroSusc, svg);
+    //Creamos 3 circulos
+    let circulos = [ ["0", ySusc - 30, "60"],  [xFinSusc - 80, ySusc - 80, "40"],  [xFinSusc * 1.7, "150", "30"], [xFinSusc * 2, ySusc - 40, "60"]];
+    circulos.forEach(valores => svg.append(crearCirculo(valores)));
+
+    //Creamos los valores para la parte de abajo que será lo contrario al anterior
+    crearDisenho(1, recuadroSusc, svg);
     //Rectángulo que contendrá el div de las suscripciones
     svg.append(crearRect(["0", (recuadroSusc.y - (recuadroSusc.height / 2))], recuadroSusc.height + "px"));
 
@@ -42,6 +30,10 @@ function crearDetallesSvg() {
     let mainRect = document.querySelector("main").getBoundingClientRect();
     //Rectángulo que contendrá el div de la tienda
     svg.append(crearRect(["0", recuadroTienda.y], (mainRect.height - mainRect.y) + "px"));
+    //Le añado mas a la y de la tienda para que se acople al rectágulo
+    recuadroTienda.y +=  recuadroTienda.height / 2;
+    //Creo el diseño para la tienda
+    crearDisenho(0, recuadroTienda, svg);
 }
 
 /**
@@ -57,7 +49,7 @@ function crearCirculo(valoresCirculo) {
     //Array con las propiedades del circulo
     let propCirculo = ["cx", "cy", "r"];
     for(let i = 0; i < propCirculo.length; i++){
-        propCirculo.setAttribute(propCirculo, valoresCirculo);
+        circulo.setAttribute(propCirculo[i], valoresCirculo[i]);
     }
     return circulo;
 }
@@ -72,6 +64,31 @@ function crearPath(valorD, clase = null) {
     }
     
     return recorrido;
+}
+
+function crearDisenho(direccion, elementoRef, svg) {
+    let banderas = !direccion ? [1, 0] : [0, 1];
+    let xSusc = 0;
+    //Calculo su Y mediante la altura del heigh del recuadro de suscr
+    let ySusc = !direccion ? (elementoRef.y - (elementoRef.height / 2) + 1) : elementoRef.y + elementoRef.height/2;
+    let xFinSusc = (elementoRef.width / 0.8) / 3;
+    curvatura = !direccion ? xFinSusc : xFinSusc - (130 * 2);
+    let restaCurvatura = 80;
+    let desplazamiento = 95;
+    //Diferencia que le quitaremos para hacer el recuadro que restaremos más pequeño
+    //A la vez que creamos los rectámngulos creamos también los que los "restarán" con la clase restar
+    for(let i = 0; i < 3; i++){
+        let lineaPath = `M${xSusc}, ${ySusc} a${curvatura}, ${curvatura} 0 0, ${banderas[0]} ${xFinSusc}, 0 Z`;
+        svg.append(crearPath(lineaPath));
+        //Diferencia que le quitaremos para hacer el recuadro que restaremos más pequeño
+        let curvaturaResta = `M${xSusc + restaCurvatura}, ${ySusc} a${curvatura - restaCurvatura}, ${curvatura - restaCurvatura} 0 0, ${banderas[0]} ${xFinSusc - restaCurvatura * 2}, 0 l-${desplazamiento}, 0 a${curvatura - desplazamiento - restaCurvatura}, ${curvatura - desplazamiento - restaCurvatura} 0 0, ${banderas[1]} -${(xFinSusc - desplazamiento)/2}, -0 z`;
+        svg.append(crearPath(curvaturaResta, "restar"));
+        !direccion ? curvatura -= 130 : curvatura += 130;
+        restaCurvatura -= 10;
+        desplazamiento += 30;
+        xSusc += xFinSusc;
+        xFinSusc += i % 2 != 0 ? 10 : 30;
+    }
 }
 
 /**
