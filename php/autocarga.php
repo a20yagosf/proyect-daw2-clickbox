444fdac3b3f4e4c29f57dc1180b9b6ae9ef73e77
Irea Vila Ramilo <irea.vila.ramilo@gmail.com>
2022-02-24 10:10:09 +0100

    Cambio a minúsculas del script de creación

--- php/autocarga.php
+++ php/autocarga.php
@@ -8,7 +8,7 @@
  */
 spl_autoload_register(function ($clase) {
     //Cambiamos la barra del namespace por la barra del directory separatos
-    $clase = str_replace("/", DIRECTORY_SEPARATOR, $clase);
+    $clase = str_replace("\\", DIRECTORY_SEPARATOR, $clase);
     $fichero = __DIR__ . DIRECTORY_SEPARATOR . $clase . ".php";
     //Comprobamos que exista el fichero
     if(is_file($fichero)){
