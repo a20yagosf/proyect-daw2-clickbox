fc15d8c4c6f638a2617a1484c80a5b5679f601db
Irea Vila Ramilo <irea.vila.ramilo@gmail.com>
2022-02-26 23:15:00 +0100

    Creacion generos multi seleccion

--- js/partidas.js
+++ js/partidas.js
@@ -8,7 +8,7 @@ window.onload = function () {
     //Añadimos el js de Bootstrap al final de body, lo hacemos aquí ya que necesitamos que esté la página cargada
     document.querySelector("body").append(crearScript({"src": "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", "integrity": "sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p", "crossorigin": "anonymous"}));
     //Cargamos los géneros
-    crearOptionGeneros(document.getElementById("genero"));
+    crearOptionGeneros(document.getElementById("generos"));
     cargarPartidas();
     //Añadimos el escuchador a filtro
     document.getElementById("filtro_fechas").nextElementSibling.addEventListener("click", filtarPartidas);
